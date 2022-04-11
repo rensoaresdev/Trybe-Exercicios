b6f4b6c15a1f470b5155372aa323084730147751
Renato Soares <rensoares.dev@gmail.com>
2022-04-11 11:45:52 -0300

    exerccio 4.1

---
+++ fundamentos/bloco-04-introducao-JS-e-LogicaProgramacao/dia-01-introducao-JS-e-JS-primeiros-passos/script.js
@@ -0,0 +1,14 @@
+let nome = "Renato";
+let birthYear = 2030;
+birthYear = 1962;
+const birthCity = "capitolio";
+
+console.log(nome);
+console.log(birthYear);
+console.log(birthCity);
+
+const base = 5;
+const height = 8;
+const area = (base * height);
+
+console.log(area);
