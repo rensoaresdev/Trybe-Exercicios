706721c45f580df6328e4eab0d267ad7bd1077a7
Renato Soares <rensoares.dev@gmail.com>
2022-04-12 11:15:19 -0300

    exercicios 1

---
+++ fundamentos/bloco-04-introducao-JS-e-LogicaProgramacao/dia-01-introducao-JS-e-JS-primeiros-passos/operacoes.js
@@ -0,0 +1,15 @@
+const a = 10;
+const b = 5;
+
+if(a + b){
+    console.log(a + b);
+}
+if(a-b){
+    console.log(a - b); 
+}
+if(a * b){
+    console.log(a * b);
+}
+if(a-b){
+    console.log(a / b); 
+}
